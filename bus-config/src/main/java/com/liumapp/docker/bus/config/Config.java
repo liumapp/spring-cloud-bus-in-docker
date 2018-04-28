021f48aa59c0722ea848ce6faadc9013c5db956a
liumapp <liumapp.com@gmail.com>
2018-04-28 10:38:45 +0800

    update bus-config package

--- bus-config/src/main/java/com/liumapp/docker/bus/config/Config.java
+++ bus-config/src/main/java/com/liumapp/docker/bus/config/Config.java
@@ -1,4 +1,4 @@
-package com.liumapp.demo.bus.config;
+package com.liumapp.docker.bus.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
