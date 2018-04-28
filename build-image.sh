f48d298347bad53886571220afecc9641da45c4d
liumapp <liumapp.com@gmail.com>
2018-04-28 11:27:16 +0800

    build image done icluding rabbitmq:3.7.4

--- build-image.sh
+++ build-image.sh
@@ -12,3 +12,67 @@ echo '$                                                           $'
 echo '============================================================='
 echo '.'
 
+# first of all , we need install rabbitMq image from docker hub .
+
+docker pull rabbitmq:3.7.4
+
+# secondly , we build our own image from project .
+
+# and pay attention plz , if you got any engine modules , plz run the commond below first
+
+cd bus-engine
+
+mvn clean install -Dmaven.test.skip=true
+
+cd ..
+
+cd bus-config
+
+mvn clean package docker:build
+
+cd ..
+
+cd bus-eureka
+
+mvn clean package docker:build
+
+cd ..
+
+cd bus-gateway
+
+mvn clean package docker:build
+
+cd ..
+
+cd bus-hello
+
+mvn clean package docker:build
+
+cd ..
+
+cd bus-service
+
+cd demo-api-a
+
+mvn clean package docker:build
+
+cd ..
+
+cd demo-api-b
+
+mvn clean package docker:build
+
+cd ..
+
+cd independent-worker
+
+mvn clean package docker:build
+
+cd ..
+
+cd ..
+
+
+
+
+
